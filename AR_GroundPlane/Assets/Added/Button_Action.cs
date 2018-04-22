7da3c142fd0a8003da7f11b42402a57e1b26411a
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-04-22 12:57:42 -0400

    adding project

---
+++ AR_GroundPlane/Assets/Added/Button_Action.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_Action : MonoBehaviour {
+
+    public GameObject Model;
+
+    private float sx,sy,sz;
+    private float speed = 1.1f;
+
+    private void get_scale()
+    {
+        sx = Model.transform.localScale.x;
+        sy = Model.transform.localScale.y;
+        sz = Model.transform.localScale.z;
+    }
+
+    private void set_scale()
+    {
+        Model.transform.localScale = new Vector3(sx, sy, sz);
+    }
+
+    public void Zoomin()
+    {
+        get_scale();
+        sx *= speed;
+        sy *= speed; 
+        sz *= speed;
+        set_scale();
+    }
+
+    public void Zoomout()
+    {
+        get_scale();
+        sx /= speed;
+        sy /= speed;
+        sz /= speed;
+        set_scale();
+    }
+}
